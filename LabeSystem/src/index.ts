5c3423757564e7e68ff26afb18dc3d930a4380c1
Leletii <leticia.nos@gmail.com>
2021-12-10 18:46:15 -0300

    tabela professores

--- LabeSystem/src/index.ts
+++ LabeSystem/src/index.ts
@@ -5,10 +5,14 @@ import { createClass } from "./endpoints/createClass";
 import { createStudent } from "./endpoints/createStudent";
 import { createTeacher } from "./endpoints/createTeacher";
 import { getStudentAgeById } from "./endpoints/getStudentAgeById";
+import  getTeacher from "./endpoints/getTeacher";
 
 app.get("/student/:id", getStudentAgeById)
+app.get("/getTeacher", getTeacher)
 app.post("/class", createClass)
 app.post("/teacher",createTeacher)
 app.post("/student",createStudent)
 app.patch("/student/:id",addStudentClass)
 app.patch("/teacher/:id", addTeacherClass)
+
+
