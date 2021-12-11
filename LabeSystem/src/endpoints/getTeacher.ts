425526825748f2a7b822cf39f9a2c72701497bcf
Leletii <leticia.nos@gmail.com>
2021-12-11 10:17:07 -0300

    tabela dos professores

--- LabeSystem/src/endpoints/getTeacher.ts
+++ LabeSystem/src/endpoints/getTeacher.ts
@@ -3,8 +3,21 @@ import connection from "../conection"
 
 export default async function getTeacher(req: Request, res: Response){
     try{
+
+        const id = req.params.id
+
         //consultar banco de dados
-        const teacher = await connection('labeSystem_docente').select('*')
+        const teacher = await connection('labeSystem_docente')
+        .select()
+        .where({
+            id : id
+        })
+
+        if(!teacher[0]) {
+            res.status(404)
+            throw new Error("Professor(a) não encontrado(a)!")
+        }
+
         //responder a requisição
         res.status(200)
         .send(teacher)
